1b2ca491b767bfa3b6a8996ff15b646337cb4623
carlykendeigh <carlykendeigh@gmail.com>
2024-03-19 13:22:45 -0500

    BANK GENERATED
    fixing how it looks!

--- src/components/Domino.js
+++ src/components/Domino.js
@@ -1,14 +1,8 @@
 import React from "react";
 import "./Domino.css";
 
-//<Domino top={3} bottom={2} />
 function Domino({ top, bottom }) {
   return (
-    // <div className="domino">
-    //   <div className="number">{top}</div>
-    //   <div className="line"></div>
-    //   <div className="number">{bottom}</div>
-    // </div>
     <div className="domino">
       <div className="topnumber">{top}</div>
       <div className="line"></div>
--- src/components/DominoBank.js
+++ src/components/DominoBank.js
@@ -2,18 +2,34 @@ import React from "react";
 import NavBar from "./NavBar";
 import Background from "./Background";
 import "./DominoBank.css";
+import Domino from "./Domino";
 
 function DominoBank() {
+  const dominos = [];
+
+  for (let top = 0; top < 13; top++) {
+    for (let top = 0; top <= 12; top++) {
+      for (let bottom = 0; bottom <= 12; bottom++) {
+        const key = `${top}-${bottom}`;
+        dominos.push(<Domino key={key} top={top} bottom={bottom} />);
+      }
+    }
+  }
+
   return (
-    <div className="dashboard-page full-page">
+    <div className="domino-page full-page">
       <Background />
       <NavBar />
       <div className="centered-content">
         <div className="container">
-          <div className="dashboard-section left">
-            <div className="navigation-buttons">
-              <h2 className="dashboard-title">Bank</h2>
-            </div>
+          <h2 className="domino-title">Bank</h2>
+
+          <div className="bank">
+            {dominos.map((domino, index) => (
+              <div key={index} className="domino-item">
+                {domino}
+              </div>
+            ))}
           </div>
         </div>
       </div>
