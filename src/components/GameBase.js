f68623bbd065dfb7ea5bce61f3db770102a732d0
carlykendeigh <carlykendeigh@gmail.com>
2024-03-24 21:38:53 -0500

    updated css and photo upload

--- src/components/GameBase.js
+++ src/components/GameBase.js
@@ -8,9 +8,14 @@ function GameChoice({ src, alt, onSelect, isSelected }) {
       <div className="full-page">
         <Background />
         <NavBar />
-        <div className="bank">
-          <h1 className="h1">Bank</h1>
-          <div> dominos here </div>
+        <div className="content">
+          <div className="sidegroup">
+            <div className="bank">
+              <h1 className="h1">Bank</h1>
+              <div> dominos here </div>
+            </div>
+            <img src="./trainstation.png" alt="domino train station" />
+          </div>
         </div>
       </div>
     </>
