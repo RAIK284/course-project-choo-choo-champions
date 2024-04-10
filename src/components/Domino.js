765f34eb57d9fd3b7b39cf68d015e398198fca27
carlykendeigh <carlykendeigh@gmail.com>
2024-04-09 21:59:55 -0500

    i am so lucky this worked so well on the first try i could cry thank you code gods

--- src/components/Domino.js
+++ src/components/Domino.js
@@ -1,61 +1,67 @@
 import React, { useState } from "react";
 import "./Domino.css";
 
-export function ConvertToReact(dominos){
-  const reactDominos = [];
-  for (let i=0;i<dominos.length;i++){
-    const domino = dominos[i];
-    reactDominos.push(<Domino key={domino[0]} top={domino[1]} bottom={domino[2]} />);
-  }
-  return reactDominos
+export function ConvertToReact(dominos) {
+  const [selectedDomino, setSelectedDomino] = useState(null);
+
+  const handleSelectDomino = (index) => {
+    setSelectedDomino(index);
+  };
+
+  const reactDominos = dominos.map((domino, index) => (
+    <Domino
+      key={domino[0]}
+      top={domino[1]}
+      bottom={domino[2]}
+      isSelected={index === selectedDomino}
+      onSelect={() => handleSelectDomino(index)}
+    />
+  ));
+  return reactDominos;
 }
+const getColor = (number) => {
+  switch (number) {
+    case 0:
+      return "black";
+    case 1:
+      return "#61A7CF";
+    case 2:
+      return "#6DD477";
+    case 3:
+      return "#C10F0F";
+    case 4:
+      return "#542E26";
+    case 5:
+      return "#436BB9";
+    case 6:
+      return "#FFB82F";
+    case 7:
+      return "#CC357E";
+    case 8:
+      return "#28A9A8";
+    case 9:
+      return "#6D0A86";
+    case 10:
+      return "#4C1A0A";
+    case 11:
+      return "#FF7E21";
+    case 12:
+      return "#909090";
+  }
+};
 
-function Domino({ top, bottom }) {
-  const [isSelected, setIsSelected] = useState(false);
-  // const getColor = (number) => {
-  //   switch (number) {
-  //     case 0:
-  //       return "black";
-  //     case 1:
-  //       return "#61A7CF";
-  //     case 2:
-  //       return "#6DD477";
-  //     case 3:
-  //       return "#C10F0F";
-  //     case 4:
-  //       return "#542E26";
-  //     case 5:
-  //       return "#436BB9";
-  //     case 6:
-  //       return "#FFB82F";
-  //     case 7:
-  //       return "#CC357E";
-  //     case 8:
-  //       return "#28A9A8";
-  //     case 9:
-  //       return "#6D0A86";
-  //     case 10:
-  //       return "#4C1A0A";
-  //     case 11:
-  //       return "#FF7E21";
-  //     case 12:
-  //       return "#909090";
-  //     default:
-  //       return "black";
-  //   }
-
+function Domino({ top, bottom, isSelected, onSelect }) {
   const getDots = (number) => {
     return `./assets/dots${number}.png`;
   };
 
+  const topNumberStyle = {
+    color: getColor(top),
+  };
 
-  // const topNumberStyle = {
-  //   color: getColor(top),
-  // };
-
-  // const bottomNumberStyle = {
-  //   color: getColor(bottom),
-  // };
+  const bottomNumberStyle = {
+    color: getColor(bottom),
+  };
 
   const topNumberDots = {
     color: getDots(top),
@@ -65,22 +71,26 @@ function Domino({ top, bottom }) {
     color: getDots(bottom),
   };
 
-  const handleClick = () => {
-    setIsSelected((prevState) => !prevState);
-  };
-
   return (
     <div
       className={`domino ${isSelected ? "selected" : ""}`}
-      onClick={handleClick}
+      onClick={onSelect}
     >
-      <img className="dominoImageTop" src={topNumberDots.color} alt="Top dots representing a number" />
+      <img
+        className="dominoImageTop"
+        src={topNumberDots.color}
+        alt="Top dots representing a number"
+      />
       <div className="line" aria-hidden="true"></div>
-      <img className="dominoImageBottom" src={bottomNumberDots.color} alt="Bottom dots representing a number" />
-
-
+      <img
+        className="dominoImageBottom"
+        src={bottomNumberDots.color}
+        alt="Bottom dots representing a number"
+      />
     </div>
   );
+
+  // for text color instead of dots
 
   //   <div className="domino">
   //   <div className="topnumber" style={topNumberStyle}>
