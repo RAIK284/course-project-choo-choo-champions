cf6af94eb335c6ba97730c0912bd106af3911965
carlykendeigh <carlykendeigh@gmail.com>
2024-04-10 22:08:57 -0500

    ABSOLUTE TRAIN POS!!!!

--- src/components/GameBase.js
+++ src/components/GameBase.js
@@ -67,6 +67,36 @@ function GameChoice({ src, alt, onSelect, isSelected }) {
                 alt="domino train station"
               />
               <div className="StartingDomino">{sDomino}</div>
+              <img
+                className="first-train"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/d737771a839e9d3381cf8be0888aafeb9423dad94e31ce6e6b57702a2eb9bb23"
+                alt="Red Train"
+              />
+              <img
+                className="second-train"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/6f86f2b40e7a9d4e455f9a504dc5e366503ace6cf85fc3fb36aac70ac88a8fdf"
+                alt="Green Train"
+              />
+              <img
+                className="third-train"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/5ec4c3ceb8e14edecf8b637152c22aeb4571947ce4e06158fb90c3aa98a3f917"
+                alt="Purple Train"
+              />
+              <img
+                className="fourth-train"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/b113a030456c013a7e21d15ec68b8ef946e9a436bdd585825811f93c41853999"
+                alt="Blue Train"
+              />
+              {/* <img
+                className="game-image-choice"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/19af73f19f2af54ce737c599541168e9050396e728f163bd56c5c822021bf5ac"
+                alt="Pink Train"
+              /> */}
+              <img
+                className="mexican-train"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/2e707c1c15ef3106cc95045c56346e6166b027d11a3d59268444fa4919181093"
+                alt="Orange Train"
+              />
               <button className="button" onClick={finishTurn}>
                 Finish Turn
               </button>{" "}
