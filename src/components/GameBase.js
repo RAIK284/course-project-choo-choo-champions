06f4a04dddf859799b0033878270da07411b180c
carlykendeigh <carlykendeigh@gmail.com>
2024-04-10 21:25:03 -0500

    it is turn

--- src/components/GameBase.js
+++ src/components/GameBase.js
@@ -1,23 +1,27 @@
 import NavBar from "./NavBar";
 import Background from "./Background";
-import { GenerateDominoesForPlayers, GeneratePathsForGame, DeterminePlayablePaths} from "./GameLogic";
+import {
+  GenerateDominoesForPlayers,
+  GeneratePathsForGame,
+  DeterminePlayablePaths,
+} from "./GameLogic";
 import { ConvertToReact } from "./Domino";
 import "./GameBase.css";
 
 function GameChoice({ src, alt, onSelect, isSelected }) {
   const players = ["max", "arjun", "carly"];
-  const startingDomino = [[90,12,12]];
-  if(sessionStorage.getItem("Player Dominoes") == null){
+  const startingDomino = [[90, 12, 12]];
+  if (sessionStorage.getItem("Player Dominoes") == null) {
     GenerateDominoesForPlayers(players, startingDomino);
   }
   const playerDominoes = JSON.parse(sessionStorage.getItem("Player Dominoes"));
-  if(sessionStorage.getItem("Player Paths") == null){
-    GeneratePathsForGame(startingDomino, players)
+  if (sessionStorage.getItem("Player Paths") == null) {
+    GeneratePathsForGame(startingDomino, players);
   }
   //const playerPaths = JSON.parse(sessionStorage.getItem("Player Paths"));
   //const dominos = ConvertToReact(playerPaths["Starting Domino"]);
-  const dominos = ConvertToReact(playerDominoes['carly']);
-  console.log(DeterminePlayablePaths('carly', players));
+  const dominos = ConvertToReact(playerDominoes["carly"]);
+  console.log(DeterminePlayablePaths("carly", players));
   return (
     <>
       <div className="full-page">
@@ -31,6 +35,17 @@ function GameChoice({ src, alt, onSelect, isSelected }) {
               <button className="button">Draw</button>
             </div>
             {/* end of left content */}
+            <div className="inner-content">
+              {/* CARLY FIX THIS!! */}
+              <h3 className="players_turn">It is turn</h3>
+              <img
+                className="trainstation"
+                src="./trainstation.png"
+                alt="domino train station"
+              />
+              {/* CARLY FIX THIS!! */}
+              <button className="button">Finish Turn</button>{" "}
+            </div>
           </div>
           {/* end of right content  */}
           {/* end of horizontal group */}
