fd52dd0d33db65d26c8c31e6ad59d5bcb6731990
carlykendeigh <carlykendeigh@gmail.com>
2024-04-18 19:11:31 -0500

    passing test

--- tests/HostGamePage.test.js
+++ tests/HostGamePage.test.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { render, fireEvent, act } from "@testing-library/react";
+import { screen, render, fireEvent, act } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import HostGamePage from "../src/components/HostGamePage";
+import { MemoryRouter } from "react-router-dom";
 
 describe("HostGamePage component", () => {
   beforeEach(() => {
@@ -10,12 +11,23 @@ describe("HostGamePage component", () => {
   afterEach(() => {
     sessionStorage.clear();
   });
-});
 
-test("renders without crashing", () => {
-  render(
-    <Router>
-      <HostGamePage />
-    </Router>
-  );
+  test("renders without crashing", () => {
+    render(
+      <Router>
+        <HostGamePage />
+      </Router>
+    );
+  });
+  test("renders join code", () => {
+    render(
+      <MemoryRouter>
+        {" "}
+        <HostGamePage />
+      </MemoryRouter>
+    );
+    const joinCodeElement = screen.getByText(/Join Code/i);
+    console.log(joinCodeElement.textContent);
+    expect(joinCodeElement.textContent.trim()).toEqual("Join Code:");
+  });
 });
