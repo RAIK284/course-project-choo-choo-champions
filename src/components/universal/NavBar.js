730b830ce7399f0d1480046ffe47a20dc5eb23ec
carlykendeigh <carlykendeigh@gmail.com>
2024-04-23 17:19:01 -0500

    nav bar reroutes corectly

--- src/components/universal/NavBar.js
+++ src/components/universal/NavBar.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SettingsButton from "../universal/SettingsButton";
-import './NavBar.css';
+import "./NavBar.css";
 
 function NavBar() {
   return (
     <header className="game-header">
       <div className="left-content">
-        <Link to="/" className="game-title">Mexican Train Dominoes</Link>
+        <Link to="/dashboard" className="game-title">
+          Mexican Train Dominoes
+        </Link>
       </div>
       <div className="right-content">
         <Link to="/profile" className="settings-link">
